6e313b190abfb299720ed2c2bedcfff7f2ec55de
Tetsuya Mori <monry84@gmail.com>
2018-02-14 22:43:51 +0900

    :sparkles: Remove implementation for Singleton
    * UniRx 内部の実装に併せて、不用意に Singleton パターンを用いない

--- Assets/Scripts/UniRx/UnityEngineBridge/Triggers/ObservableKeyEvent.cs
+++ Assets/Scripts/UniRx/UnityEngineBridge/Triggers/ObservableKeyEvent.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityModule;
+
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable UnusedMember.Global
 
 namespace UniRx.Triggers {
 
-    public class ObservableKeyEvent : Singleton<ObservableKeyEvent> {
+    public static class ObservableKeyEvent {
 
         private static readonly Dictionary<KeyEventType, Func<KeyCode, bool>> DELEGATE_MAP = new Dictionary<KeyEventType, Func<KeyCode, bool>>() {
             { KeyEventType.Key, Input.GetKey },
@@ -21,38 +21,20 @@ namespace UniRx.Triggers {
             KeyUp,
         }
 
-        private Dictionary<KeyEventType, Dictionary<KeyCode, IObservable<Unit>>> streamMap;
-
-        private Dictionary<KeyEventType, Dictionary<KeyCode, IObservable<Unit>>> StreamMap {
-            get {
-                if (this.streamMap == default(Dictionary<KeyEventType, Dictionary<KeyCode, IObservable<Unit>>>)) {
-                    this.streamMap = new Dictionary<KeyEventType, Dictionary<KeyCode, IObservable<Unit>>>() {
-                        { KeyEventType.Key, new Dictionary<KeyCode, IObservable<Unit>>() },
-                        { KeyEventType.KeyDown, new Dictionary<KeyCode, IObservable<Unit>>() },
-                        { KeyEventType.KeyUp, new Dictionary<KeyCode, IObservable<Unit>>() },
-                    };
-                }
-                return this.streamMap;
-            }
+        public static IObservable<Unit> OnKeyAsObservable(KeyCode keyCode) {
+            return GetOrCreateSubject(KeyEventType.Key, keyCode);
         }
 
-        public IObservable<Unit> OnKeyAsObservable(KeyCode keyCode) {
-            return this.GetOrCreateSubject(KeyEventType.Key, keyCode);
+        public static IObservable<Unit> OnKeyDownAsObservable(KeyCode keyCode) {
+            return GetOrCreateSubject(KeyEventType.KeyDown, keyCode);
         }
 
-        public IObservable<Unit> OnKeyDownAsObservable(KeyCode keyCode) {
-            return this.GetOrCreateSubject(KeyEventType.KeyDown, keyCode);
+        public static IObservable<Unit> OnKeyUpAsObservable(KeyCode keyCode) {
+            return GetOrCreateSubject(KeyEventType.KeyUp, keyCode);
         }
 
-        public IObservable<Unit> OnKeyUpAsObservable(KeyCode keyCode) {
-            return this.GetOrCreateSubject(KeyEventType.KeyUp, keyCode);
-        }
-
-        private IObservable<Unit> GetOrCreateSubject(KeyEventType keyEventType, KeyCode keyCode) {
-            if (!this.StreamMap[keyEventType].ContainsKey(keyCode)) {
-                this.StreamMap[keyEventType][keyCode] = Observable.EveryUpdate().Where(_ => DELEGATE_MAP[keyEventType](keyCode)).AsUnitObservable();
-            }
-            return this.StreamMap[keyEventType][keyCode];
+        private static IObservable<Unit> GetOrCreateSubject(KeyEventType keyEventType, KeyCode keyCode) {
+            return Observable.EveryUpdate().Where(_ => DELEGATE_MAP[keyEventType](keyCode)).AsUnitObservable();
         }
 
     }
@@ -60,15 +42,15 @@ namespace UniRx.Triggers {
     public static class KeyEventComponentExtension {
 
         public static IObservable<Unit> OnKeyAsObservable(this Component component, KeyCode keyCode) {
-            return ObservableKeyEvent.Instance.OnKeyAsObservable(keyCode).TakeUntilDestroy(component);
+            return ObservableKeyEvent.OnKeyAsObservable(keyCode).TakeUntilDestroy(component);
         }
 
         public static IObservable<Unit> OnKeyDownAsObservable(this Component component, KeyCode keyCode) {
-            return ObservableKeyEvent.Instance.OnKeyDownAsObservable(keyCode).TakeUntilDestroy(component);
+            return ObservableKeyEvent.OnKeyDownAsObservable(keyCode).TakeUntilDestroy(component);
         }
 
         public static IObservable<Unit> OnKeyUpAsObservable(this Component component, KeyCode keyCode) {
-            return ObservableKeyEvent.Instance.OnKeyUpAsObservable(keyCode).TakeUntilDestroy(component);
+            return ObservableKeyEvent.OnKeyUpAsObservable(keyCode).TakeUntilDestroy(component);
         }
 
     }
